b9cd4e1f4d19c0074e52e868e8de18f031801de9
Demian Veenhoven <demian.veenhoven@gmail.com>
2021-07-22 13:24:56 +0200

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <yahtzee/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Yahtzee from './components/Yahtzee.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Yahtzee
   }
 }
 </script>
---
+++ src/components/Yahtzee.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="Yahtzee">
+    <h1>Yahtzee</h1>
+    <table>
+    </table>
+
+    <table>
+      <tr>
+        <th colspan="6">
+          Thrown numbers
+        </th>
+      </tr>
+      <tr>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+        <th>6</th>
+      </tr>
+      <tr>
+        <td v-for="number in sortedDice" :key="number.value">{{number}}</td>
+      </tr>
+    </table>
+    <br>
+
+    <table>
+      <tr>
+        <th></th>
+        <th>Score</th>
+      </tr>
+      <tr>
+        <td>Three of a Kind</td>
+        <td>{{scoreThreeOfAkind}}</td>
+      </tr>
+      <tr>
+        <td>Four of a Kind</td>
+        <td>{{scoreFourOfAkind}}</td>
+      </tr>
+      <tr>
+        <td>Small Street</td>
+        <td>{{scoreSmallStreet}}</td>
+      </tr>
+      <tr>
+        <td>Large Street</td>
+        <td>{{scoreLargeStreet}}</td>
+      </tr>
+      <tr>
+        <td>Full House</td>
+        <td>{{scoreFullHouse}}</td>
+      </tr>
+      <tr>
+        <td>Chance</td>
+        <td>{{total}}</td>
+      </tr>
+      <tr>
+        <td>Yahtzee</td>
+        <td>{{scoreYahtzee}}</td>
+      </tr>
+    </table>
+
+    <button @click="throwDice">
+      Throw dice!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Yahtzee',
+
+  data() {
+    return {
+      thrownNumbers: [],
+    }
+  },
+
+  computed:{
+    total() {
+      return this.thrownNumbers.reduce((acc,d)=>acc+=d,0);
+    },
+
+    noDuplicates() {
+      let noDuplicates = [...new Set(this.thrownNumbers)]
+      console.log(noDuplicates);
+      return noDuplicates;
+    },
+
+    sortedDice() {
+      if(this.thrownNumbers.length == 0) return []
+
+      let sortedNumbers = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
+      };
+
+      for (const number in sortedNumbers) {
+        sortedNumbers[number] = this.thrownNumbers.filter(die => die == number).length   
+      }
+
+      return sortedNumbers;
+    },
+
+    scoreThreeOfAkind() {
+      for (const number in this.sortedDice) {
+        if (this.sortedDice[number] >= 3) {
+          return this.total;
+        }
+      }
+      return 0;
+    },
+
+    scoreFourOfAkind() {
+      for (const number in this.sortedDice) {
+        if (this.sortedDice[number] >= 4) {
+          return this.total;
+        }
+      }
+      return 0;
+    },
+
+    scoreSmallStreet() {
+      let smallStreetProgress = 0;
+      if (this.noDuplicates.length >= 4) {
+        for (let i = 1; i < this.noDuplicates.length; i++) {
+          if (this.noDuplicates[i] == (this.noDuplicates[i-1]+1)) {
+            smallStreetProgress++;
+          }
+        }
+        if(smallStreetProgress >= 3) {
+          return 30;
+        }
+      }
+      return 0;
+    },
+
+    scoreLargeStreet() {
+      let largeStreetProgress = 1;
+      if (this.noDuplicates.length == 5) {
+        for (let i = 1; i < this.noDuplicates.length; i++) {
+          if (this.noDuplicates[i] != (this.noDuplicates[i-1]+1)) {
+            largeStreetProgress--;
+          }
+        }
+        if(largeStreetProgress == 1) {
+          return 40;
+        }
+      }
+      return 0;
+    },
+
+    scoreFullHouse() {
+      if (this.noDuplicates.length == 2) {
+        for (const number in this.sortedDice) {
+          if (this.sortedDice[number] >= 3) {
+            for (const number2 in this.sortedDice) {
+              if (this.sortedDice[number2] == 2) {
+                return 25;
+              }
+            }
+          }
+        }
+      }
+      return 0;
+    },
+
+    scoreYahtzee() {
+      for (const number in this.sortedDice) {
+        if (this.sortedDice[number] == 5) {
+          return 50;
+        }
+      }
+      return 0;
+    }
+  },
+
+  methods: {
+    throwDice() {
+      this.thrownNumbers = [];
+      for (let i=1; i < 6; i++) {
+        this.thrownNumbers.push(Math.ceil(Math.random()*6));
+      }
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table, th, td {
+  padding: 10px;
+  border: 1px solid black;
+  border-collapse: collapse;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
